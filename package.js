852af6daed319a96234db93251befa61710aef2d
Yacine Merzouk <yacine@merzouk.ca>
2022-08-20 22:02:53 -0400

    1.0.10 Udpated package dependencies

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'meteorhubdotnet:firestorm-helpers',
     summary: 'Helper functions for Firestorm. Includes Iron Router and Blaze helpers.',
-    version: '1.0.7',
+    version: '1.0.10',
     git: 'https://github.com/meteorhubdotnet/firestorm-helpers',
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
     api.versionsFrom('METEOR@2.7.3');
 
     api.use([
-        'meteorhubdotnet:firestorm-iron-router@1.0.7',
+        'meteorhubdotnet:firestorm-iron-router@1.0.10',
         'blaze@2.6.1',
         // 'templating@1.4.2',
         'modules',
